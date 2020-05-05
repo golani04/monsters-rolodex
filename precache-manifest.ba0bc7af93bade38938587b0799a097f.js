d148e32e5387f7e830618b9a8eb75de9af59837a
Leonid <golani04@gmail.com>
2020-05-05 11:12:17 +0300

    Updates

---
+++ precache-manifest.ba0bc7af93bade38938587b0799a097f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1608ea823d241823b5dcb7ee6be7b3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "54bef5fc7019f40730ba",
+    "url": "/monsters-rolodex/static/css/main.52b76700.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54bef5fc7019f40730ba",
+    "url": "/monsters-rolodex/static/js/main.22193348.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
